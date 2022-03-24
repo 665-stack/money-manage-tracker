b4e0f3f4c7c319ca7def8d1c83527827a4f300cf
unknown <mahmodulshahin665@gmail.com>
2022-03-24 23:59:04 +0600

    add save btn handler

--- js/script.js
+++ js/script.js
@@ -11,30 +11,25 @@ function expensesCalc() {
     const expensesSum = foodInput + rentInput + clothsInput;
     return expensesSum;
 }
-// function updateTotal() {
-//     const income = getInputValue();
-//     const expenses = expensesCalc();
-//     const minus = income + expenses;
-//     console.log(minus)
-
-//     return minus;
-// }
-function getCurrentBalance() {
-    const totalBalance = document.getElementById('total-balance');
-    const totalBalanceText = totalBalance.innerText;
-    const previousTotal = parseFloat(totalBalanceText);
-    return previousTotal;
-}
+// calculator button
 document.getElementById('calc-btn').addEventListener('click', function () {
-    const incomeInput = getInputValue('income-input');
-    console.log('incomeInput -- ', incomeInput);
-    const getExpenses = expensesCalc();
-
-    console.log('getExpenses -- ', getExpenses);
-    const getUpdateTotal = incomeInput - getExpenses;
-    const Balance = getCurrentBalance();
+    const income = getInputValue('income-input');
+    const expenses = expensesCalc();
+    //update total expenses
+    const totalExpenses = document.getElementById('total-expenses');
+    totalExpenses.innerText = expenses;
+    //update balance
+    const UpdateBalanceTotal = income - expenses;
     const totalBalance = document.getElementById('total-balance');
-    totalBalance.innerText = getUpdateTotal;
-
-    console.log('getUpdateTotal -- ', getUpdateTotal)
+    totalBalance.innerText = UpdateBalanceTotal;
 })
+// save button
+document.getElementById('save-btn').addEventListener('click', function () {
+    const saveInput = getInputValue('save-input');
+    const income2 = getInputValue('income-input');
+    const expenses = expensesCalc();
+    const updateBlcTotal = income2 - expenses;
+    const saveMoney = updateBlcTotal * saveInput;
+    saveInput.innerText = saveMoney;
+    console.log(saveMoney)
+});
