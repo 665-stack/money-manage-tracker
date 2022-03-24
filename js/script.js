b2288d1e103159587ecf95f4eee37263b1072645
unknown <mahmodulshahin665@gmail.com>
2022-03-24 22:50:40 +0600

    add total balance calc

--- js/script.js
+++ js/script.js
@@ -4,16 +4,37 @@ function getInputValue(inputId) {
     const amountValue = parseFloat(inputAmountText);
     return amountValue;
 }
-function expensesCalc(inputField) {
+function expensesCalc() {
+    const foodInput = getInputValue('food-input');
+    const rentInput = getInputValue('rent-input');
+    const clothsInput = getInputValue('cloths-input');
+    const expensesSum = foodInput + rentInput + clothsInput;
+    return expensesSum;
+}
+// function updateTotal() {
+//     const income = getInputValue();
+//     const expenses = expensesCalc();
+//     const minus = income + expenses;
+//     console.log(minus)
 
+//     return minus;
+// }
+function getCurrentBalance() {
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceText = totalBalance.innerText;
+    const previousTotal = parseFloat(totalBalanceText);
+    return previousTotal;
 }
 document.getElementById('calc-btn').addEventListener('click', function () {
     const incomeInput = getInputValue('income-input');
-    console.log('incomeInput -- ', incomeInput)
-    const foodInput = getInputValue('food-input');
-    console.log('fontInput -- ', foodInput)
-    const rentInput = getInputValue('rent-input');
-    console.log('rentInput -- ', rentInput);
-    const clothsInput = getInputValue('cloths-input');
-    console.log('clothsInput -- ', clothsInput)
+    console.log('incomeInput -- ', incomeInput);
+    const getExpenses = expensesCalc();
+
+    console.log('getExpenses -- ', getExpenses);
+    const getUpdateTotal = incomeInput - getExpenses;
+    const Balance = getCurrentBalance();
+    const totalBalance = document.getElementById('total-balance');
+    totalBalance.innerText = getUpdateTotal;
+
+    console.log('getUpdateTotal -- ', getUpdateTotal)
 })
